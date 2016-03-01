23f328b1abb8e21942fb1c23a67ab18304674c4d
Johnny Goodnow <johnnygoodnow@asana.com>
2016-03-01 11:37:24 -0800

    Add PyPI classifiers and additional metadata.

--- setup.py
+++ setup.py
@@ -4,24 +4,38 @@ import sys
 import os
 from setuptools import setup, find_packages
 
+from version import VERSION
+
 assert sys.version_info >= (2, 6), 'We only support Python 2.6+'
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'asana'))
-from version import VERSION
 
-setup(name='asana',
-      version=VERSION,
-      description='Asana API client',
-      # license='',
-      install_requires=[
-          'requests~=2.9.1',
-          'requests_oauthlib~=0.6.1',
-          'six~=1.10.0'
-      ],
-      author='Asana, Inc',
-      # author_email='',
-      url='http://github.com/asana/python-asana',
-      packages = find_packages(),
-      keywords= 'asana',
-      zip_safe = True,
-      test_suite='tests')
+setup(
+    name='asana',
+    version=VERSION,
+    description='Asana API client',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ],
+    install_requires=[
+        'requests~=2.9.1',
+        'requests_oauthlib~=0.6.1',
+        'six~=1.10.0'
+    ],
+    author='Asana, Inc',
+    # author_email='',
+    url='http://github.com/asana/python-asana',
+    packages=find_packages(exclude=('tests',)),
+    keywords='asana',
+    zip_safe=True,
+    test_suite='tests')
