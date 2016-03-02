b30b8d84aed7d755876bb99ac349796c859b2fbe
Johnny Goodnow <johnnygoodnow@asana.com>
2016-03-02 15:32:39 -0800

    Fix new version script and versioning for setup.py.
    * Version is no longer in its own file, but now exists in __init__.py
    * Version is pulled into setup.py via reading __init__.py and parsing
      via regex (the re module will always be installed, and it avoids
      importing 3rd party dependencies).
    * Deploy script now modifiys the version in __init__.py.
    * Added additional metadata to the package.

--- asana/__init__.py
+++ asana/__init__.py
@@ -1,4 +1,6 @@
 from .client import Client
-from .version import VERSION
 
-__version__ = VERSION
+__title__ = 'asana'
+__version__ = '0.5.1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Asana, Inc.'
