3288497b1be56d615cce12515807fe850395e6cb
Johnny Goodnow <jgoodnow@fastmail.com>
2018-01-16 21:48:06 -0800

    Version bump to v0.6.6.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'asana'))
 
 setup(
     name='asana',
-    version='0.6.5',
+    version='0.6.6',
     description='Asana API client',
     license='MIT',
     classifiers=[
