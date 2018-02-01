a97d91732b4d3238babd298620d2219ea9691451
Johnny Goodnow <jgoodnow@fastmail.com>
2018-02-01 12:25:06 -0800

    Add additional builds and only deploy on 2.7.

--- setup.py
+++ setup.py
@@ -23,7 +23,9 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     install_requires=[
         'requests >= 2.14, == 2.*',
