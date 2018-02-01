a37872fdf082256ae50b1a01c42f5e8922612f72
Johnny Goodnow <jgoodnow@fastmail.com>
2018-02-01 12:34:07 -0800

    End support for Python 2.6.
    2.6 hasn't received any updates since 2016, and its docs are no longer
    listed on Python's website. Furthermore, our tests don't work with
    Python 2.6.
    
    We'll need to up the version number when this gets merged in.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 import os
 from setuptools import setup, find_packages
 
-assert sys.version_info >= (2, 6), 'We only support Python 2.6+'
+assert sys.version_info >= (2, 7), 'We only support Python 2.7+'
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'asana'))
 
@@ -19,7 +19,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
