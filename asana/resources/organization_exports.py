4b2789b4426654a7e3f55d4e27c5ec2912d569f5
Johnny Goodnow <jgoodnow@fastmail.com>
2018-02-08 20:12:04 -0800

    Add sections & organization exports support.

---
+++ asana/resources/organization_exports.py
@@ -0,0 +1,8 @@
+from .gen.organization_exports import _OrganizationExports
+
+
+class OrganizationExports(_OrganizationExports):
+    """The :class:`OrganizationExports` object, which represents the resource
+    of the same name in Asana's API.
+
+    """
