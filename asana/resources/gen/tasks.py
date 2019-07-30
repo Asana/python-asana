1845dfd36c127a796e998d3038401ad509c3b70e
Ross Grambo <rossgrambo@asana.com>
2019-07-30 12:40:05 -0700

    Misc. gen fixes

--- asana/resources/gen/tasks.py
+++ asana/resources/gen/tasks.py
@@ -8,16 +8,16 @@ class _Tasks:
 
     def __init__(self, client=None):
         self.client = client
-  
-    def create(self, params={}, **options): 
+
+    def create(self, params={}, **options):
         """Creating a new task is as easy as POSTing to the `/tasks` endpoint
         with a data block containing the fields you'd like to set on the task.
         Any unspecified fields will take on default values.
-        
+
         Every task is required to be created in a specific workspace, and this
         workspace cannot be changed once set. The workspace need not be set
         explicitly if you specify `projects` or a `parent` task instead.
-        
+
         `projects` can be a comma separated list of projects, or just a single
         project the task should belong to.
 
@@ -27,12 +27,12 @@ class _Tasks:
           - [workspace] : {Gid} The workspace to create a task in.
         """
         return self.client.post("/tasks", params, **options)
-        
-    def create_in_workspace(self, workspace, params={}, **options): 
+
+    def create_in_workspace(self, workspace, params={}, **options):
         """Creating a new task is as easy as POSTing to the `/tasks` endpoint
         with a data block containing the fields you'd like to set on the task.
         Any unspecified fields will take on default values.
-        
+
         Every task is required to be created in a specific workspace, and this
         workspace cannot be changed once set. The workspace need not be set
         explicitly if you specify a `project` or a `parent` task instead.
@@ -44,8 +44,8 @@ class _Tasks:
         """
         path = "/workspaces/%s/tasks" % (workspace)
         return self.client.post(path, params, **options)
-        
-    def find_by_id(self, task, params={}, **options): 
+
+    def find_by_id(self, task, params={}, **options):
         """Returns the complete task record for a single task.
 
         Parameters
@@ -55,16 +55,16 @@ class _Tasks:
         """
         path = "/tasks/%s" % (task)
         return self.client.get(path, params, **options)
-        
-    def update(self, task, params={}, **options): 
+
+    def update(self, task, params={}, **options):
         """A specific, existing task can be updated by making a PUT request on the
         URL for that task. Only the fields provided in the `data` block will be
         updated; any unspecified fields will remain unchanged.
-        
+
         When using this method, it is best to specify only those fields you wish
         to change, or else you may overwrite changes made by another user since
         you last retrieved the task.
-        
+
         Returns the complete updated task record.
 
         Parameters
@@ -74,13 +74,13 @@ class _Tasks:
         """
         path = "/tasks/%s" % (task)
         return self.client.put(path, params, **options)
-        
-    def delete(self, task, params={}, **options): 
+
+    def delete(self, task, params={}, **options):
         """A specific, existing task can be deleted by making a DELETE request on the
         URL for that task. Deleted tasks go into the "trash" of the user making
         the delete request. Tasks can be recovered from the trash within a period
         of 30 days; afterward they are completely removed from the system.
-        
+
         Returns an empty data record.
 
         Parameters
@@ -89,8 +89,8 @@ class _Tasks:
         """
         path = "/tasks/%s" % (task)
         return self.client.delete(path, params, **options)
-        
-    def duplicate_task(self, task, params={}, **options): 
+
+    def duplicate_task(self, task, params={}, **options):
         """Creates and returns a job that will asynchronously handle the duplication.
 
         Parameters
@@ -102,8 +102,8 @@ class _Tasks:
         """
         path = "/tasks/%s/duplicate" % (task)
         return self.client.post(path, params, **options)
-        
-    def find_by_project(self, project, params={}, **options): 
+
+    def find_by_project(self, project, params={}, **options):
         """Returns the compact task records for all tasks within the given project,
         ordered by their priority within the project.
 
@@ -114,8 +114,8 @@ class _Tasks:
         """
         path = "/projects/%s/tasks" % (project)
         return self.client.get_collection(path, params, **options)
-        
-    def find_by_tag(self, tag, params={}, **options): 
+
+    def find_by_tag(self, tag, params={}, **options):
         """Returns the compact task records for all tasks with the given tag.
 
         Parameters
@@ -125,8 +125,8 @@ class _Tasks:
         """
         path = "/tags/%s/tasks" % (tag)
         return self.client.get_collection(path, params, **options)
-        
-    def find_by_section(self, section, params={}, **options): 
+
+    def find_by_section(self, section, params={}, **options):
         """<b>Board view only:</b> Returns the compact section records for all tasks within the given section.
 
         Parameters
@@ -136,21 +136,21 @@ class _Tasks:
         """
         path = "/sections/%s/tasks" % (section)
         return self.client.get_collection(path, params, **options)
-        
-    def find_by_user_task_list(self, user_task_list, params={}, **options): 
+
+    def find_by_user_task_list(self, user_task_list, params={}, **options):
         """Returns the compact list of tasks in a user's My Tasks list. The returned
         tasks will be in order within each assignee status group of `Inbox`,
         `Today`, and `Upcoming`.
-        
+
         **Note:** tasks in `Later` have a different ordering in the Asana web app
         than the other assignee status groups; this endpoint will still return
         them in list order in `Later` (differently than they show up in Asana,
         but the same order as in Asana's mobile apps).
-        
+
         **Note:** Access control is enforced for this endpoint as with all Asana
         API endpoints, meaning a user's private tasks will be filtered out if the
         API-authenticated user does not have access to them.
-        
+
         **Note:** Both complete and incomplete tasks are returned by default
         unless they are filtered out (for example, setting `completed_since=now`
         will return only incomplete tasks, which is the default view for "My
@@ -165,8 +165,8 @@ class _Tasks:
         """
         path = "/user_task_lists/%s/tasks" % (user_task_list)
         return self.client.get_collection(path, params, **options)
-        
-    def find_all(self, params={}, **options): 
+
+    def find_all(self, params={}, **options):
         """Returns the compact task records for some filtered set of tasks. Use one
         or more of the parameters provided to filter the tasks returned. You must
         specify a `project`, `section`, `tag`, or `user_task_list` if you do not
@@ -186,20 +186,8 @@ class _Tasks:
           - [modified_since] : {String} Only return tasks that have been modified since the given time.
         """
         return self.client.get_collection("/tasks", params, **options)
-        
-    def get_tasks_with_tag(self, tag, params={}, **options): 
-        """Returns the compact task records for all tasks with the given tag.
-        Tasks can have more than one tag at a time.
 
-        Parameters
-        ----------
-        tag : {Id} The tag to fetch tasks from.
-        [params] : {Object} Parameters for the request
-        """
-        path = "/tags/%s/tasks" % (tag)
-        return self.client.get_collection(path, params, **options)
-        
-    def search_in_workspace(self, workspace, params={}, **options): 
+    def search_in_workspace(self, workspace, params={}, **options):
         """The search endpoint allows you to build complex queries to find and fetch exactly the data you need from Asana. For a more comprehensive description of all the query parameters and limitations of this endpoint, see our [long-form documentation](/developers/documentation/getting-started/search-api) for this feature.
 
         Parameters
@@ -210,8 +198,8 @@ class _Tasks:
         """
         path = "/workspaces/%s/tasks/search" % (workspace)
         return self.client.get_collection(path, params, **options)
-        
-    def dependencies(self, task, params={}, **options): 
+
+    def dependencies(self, task, params={}, **options):
         """Returns the compact representations of all of the dependencies of a task.
 
         Parameters
@@ -221,8 +209,8 @@ class _Tasks:
         """
         path = "/tasks/%s/dependencies" % (task)
         return self.client.get(path, params, **options)
-        
-    def dependents(self, task, params={}, **options): 
+
+    def dependents(self, task, params={}, **options):
         """Returns the compact representations of all of the dependents of a task.
 
         Parameters
@@ -232,8 +220,8 @@ class _Tasks:
         """
         path = "/tasks/%s/dependents" % (task)
         return self.client.get(path, params, **options)
-        
-    def add_dependencies(self, task, params={}, **options): 
+
+    def add_dependencies(self, task, params={}, **options):
         """Marks a set of tasks as dependencies of this task, if they are not
         already dependencies. *A task can have at most 15 dependencies.*
 
@@ -245,8 +233,8 @@ class _Tasks:
         """
         path = "/tasks/%s/addDependencies" % (task)
         return self.client.post(path, params, **options)
-        
-    def add_dependents(self, task, params={}, **options): 
+
+    def add_dependents(self, task, params={}, **options):
         """Marks a set of tasks as dependents of this task, if they are not already
         dependents. *A task can have at most 30 dependents.*
 
@@ -258,8 +246,8 @@ class _Tasks:
         """
         path = "/tasks/%s/addDependents" % (task)
         return self.client.post(path, params, **options)
-        
-    def remove_dependencies(self, task, params={}, **options): 
+
+    def remove_dependencies(self, task, params={}, **options):
         """Unlinks a set of dependencies from this task.
 
         Parameters
@@ -270,8 +258,8 @@ class _Tasks:
         """
         path = "/tasks/%s/removeDependencies" % (task)
         return self.client.post(path, params, **options)
-        
-    def remove_dependents(self, task, params={}, **options): 
+
+    def remove_dependents(self, task, params={}, **options):
         """Unlinks a set of dependents from this task.
 
         Parameters
@@ -282,8 +270,8 @@ class _Tasks:
         """
         path = "/tasks/%s/removeDependents" % (task)
         return self.client.post(path, params, **options)
-        
-    def add_followers(self, task, params={}, **options): 
+
+    def add_followers(self, task, params={}, **options):
         """Adds each of the specified followers to the task, if they are not already
         following. Returns the complete, updated record for the affected task.
 
@@ -295,8 +283,8 @@ class _Tasks:
         """
         path = "/tasks/%s/addFollowers" % (task)
         return self.client.post(path, params, **options)
-        
-    def remove_followers(self, task, params={}, **options): 
+
+    def remove_followers(self, task, params={}, **options):
         """Removes each of the specified followers from the task if they are
         following. Returns the complete, updated record for the affected task.
 
@@ -308,8 +296,8 @@ class _Tasks:
         """
         path = "/tasks/%s/removeFollowers" % (task)
         return self.client.post(path, params, **options)
-        
-    def projects(self, task, params={}, **options): 
+
+    def projects(self, task, params={}, **options):
         """Returns a compact representation of all of the projects the task is in.
 
         Parameters
@@ -319,21 +307,21 @@ class _Tasks:
         """
         path = "/tasks/%s/projects" % (task)
         return self.client.get_collection(path, params, **options)
-        
-    def add_project(self, task, params={}, **options): 
+
+    def add_project(self, task, params={}, **options):
         """Adds the task to the specified project, in the optional location
         specified. If no location arguments are given, the task will be added to
         the end of the project.
-        
+
         `addProject` can also be used to reorder a task within a project or section that
         already contains it.
-        
+
         At most one of `insert_before`, `insert_after`, or `section` should be
         specified. Inserting into a section in an non-order-dependent way can be
         done by specifying `section`, otherwise, to insert within a section in a
         particular place, specify `insert_before` or `insert_after` and a task
         within the section to anchor the position of this task.
-        
+
         Returns an empty data block.
 
         Parameters
@@ -350,11 +338,11 @@ class _Tasks:
         """
         path = "/tasks/%s/addProject" % (task)
         return self.client.post(path, params, **options)
-        
-    def remove_project(self, task, params={}, **options): 
+
+    def remove_project(self, task, params={}, **options):
         """Removes the task from the specified project. The task will still exist
         in the system, but it will not be in the project anymore.
-        
+
         Returns an empty data block.
 
         Parameters
@@ -365,8 +353,8 @@ class _Tasks:
         """
         path = "/tasks/%s/removeProject" % (task)
         return self.client.post(path, params, **options)
-        
-    def tags(self, task, params={}, **options): 
+
+    def tags(self, task, params={}, **options):
         """Returns a compact representation of all of the tags the task has.
 
         Parameters
@@ -376,8 +364,8 @@ class _Tasks:
         """
         path = "/tasks/%s/tags" % (task)
         return self.client.get_collection(path, params, **options)
-        
-    def add_tag(self, task, params={}, **options): 
+
+    def add_tag(self, task, params={}, **options):
         """Adds a tag to a task. Returns an empty data block.
 
         Parameters
@@ -388,8 +376,8 @@ class _Tasks:
         """
         path = "/tasks/%s/addTag" % (task)
         return self.client.post(path, params, **options)
-        
-    def remove_tag(self, task, params={}, **options): 
+
+    def remove_tag(self, task, params={}, **options):
         """Removes a tag from the task. Returns an empty data block.
 
         Parameters
@@ -400,8 +388,8 @@ class _Tasks:
         """
         path = "/tasks/%s/removeTag" % (task)
         return self.client.post(path, params, **options)
-        
-    def subtasks(self, task, params={}, **options): 
+
+    def subtasks(self, task, params={}, **options):
         """Returns a compact representation of all of the subtasks of a task.
 
         Parameters
@@ -411,8 +399,8 @@ class _Tasks:
         """
         path = "/tasks/%s/subtasks" % (task)
         return self.client.get_collection(path, params, **options)
-        
-    def add_subtask(self, task, params={}, **options): 
+
+    def add_subtask(self, task, params={}, **options):
         """Creates a new subtask and adds it to the parent task. Returns the full record
         for the newly created subtask.
 
@@ -423,8 +411,8 @@ class _Tasks:
         """
         path = "/tasks/%s/subtasks" % (task)
         return self.client.post(path, params, **options)
-        
-    def stories(self, task, params={}, **options): 
+
+    def stories(self, task, params={}, **options):
         """Returns a compact representation of all of the stories on the task.
 
         Parameters
@@ -434,12 +422,12 @@ class _Tasks:
         """
         path = "/tasks/%s/stories" % (task)
         return self.client.get_collection(path, params, **options)
-        
-    def add_comment(self, task, params={}, **options): 
+
+    def add_comment(self, task, params={}, **options):
         """Adds a comment to a task. The comment will be authored by the
         currently authenticated user, and timestamped when the server receives
         the request.
-        
+
         Returns the full record for the new story added to the task.
 
         Parameters
@@ -450,14 +438,14 @@ class _Tasks:
         """
         path = "/tasks/%s/stories" % (task)
         return self.client.post(path, params, **options)
-        
-    def insert_in_user_task_list(self, user_task_list, params={}, **options): 
+
+    def insert_in_user_task_list(self, user_task_list, params={}, **options):
         """Insert or reorder tasks in a user's My Tasks list. If the task was not
         assigned to the owner of the user task list it will be reassigned when
         this endpoint is called. If neither `insert_before` nor `insert_after`
         are provided the task will be inserted at the top of the assignee's
         inbox.
-        
+
         Returns an empty data block.
 
         Parameters
@@ -474,4 +462,4 @@ class _Tasks:
         """
         path = "/user_task_lists/%s/tasks/insert" % (user_task_list)
         return self.client.post(path, params, **options)
-        
+
