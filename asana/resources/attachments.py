dd960640d8e79176fb914c68143599fc9cfc7ad1
Theo Spears <theo@asana.com>
2019-09-06 14:53:25 -0700

    Files may be uploaded using a string ID
    As we are moving to string IDs, we should accept them here. %s will also allow numbers (converting them to strings) so this is backwards compatible.

--- asana/resources/attachments.py
+++ asana/resources/attachments.py
@@ -6,5 +6,5 @@ class Attachments(_Attachments):
 
     def create_on_task(self, task_id, file_content, file_name, file_content_type=None, **options):
         """Upload an attachment for a task. Accepts a file object or string, file name, and optional file Content-Type"""
-        path = '/tasks/%d/attachments' % (task_id)
+        path = '/tasks/%s/attachments' % (task_id)
         return self.client.request('post', path, files=[('file', (file_name, file_content, file_content_type))], **options)
