997558056ec40964f0fe800170f4143c1e2c1a3c
Ross Grambo <rossgrambo@asana.com>
2020-10-05 14:46:05 -0700

    Added create_attachment_for_task to attachment resource

--- asana/resources/attachments.py
+++ asana/resources/attachments.py
@@ -4,8 +4,12 @@ from .gen.attachments import _Attachments
 class Attachments(_Attachments):
     """Attachments resource"""
 
-    def create_on_task(self, task_id, file_content, file_name, file_content_type=None, **options):
+    def create_attachment_for_task(self, task_id, file_content, file_name, file_content_type=None, **options):
         """Upload an attachment for a task. Accepts a file object or string, file name, and optional file Content-Type"""
+        path = '/tasks/%s/attachments' % (task_id)
+        return self.client.request('post', path, files=[('file', (file_name, file_content, file_content_type))], **options)
+
+    def create_on_task(self, task_id, file_content, file_name, file_content_type=None, **options):
         path = '/tasks/%s/attachments' % (task_id)
         return self.client.request('post', path, files=[('file', (file_name, file_content, file_content_type))], **options)
 
