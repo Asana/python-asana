532065b9ae61dd6cdd2ccd68e3374a44e414bb39
John Vu <johnvu@asana.com>
2022-03-04 14:31:48 -0800

    Generated from OpenAPI

---
+++ asana/resources/gen/project_templates.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+class _ProjectTemplates:
+
+    def __init__(self, client=None):
+        self.client = client
+
+    def get_project_template(self, project_template_gid, params=None, **options):
+        """Get a project template
+        :param str project_template_gid: (required) Globally unique identifier for the project template.
+        :param Object params: Parameters for the request
+        :param **options
+            - opt_fields {list[str]}:  Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options.
+            - opt_pretty {bool}:  Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging.
+        :return: Object
+        """
+        if params is None:
+            params = {}
+        path = "/project_templates/{project_template_gid}".replace("{project_template_gid}", project_template_gid)
+        return self.client.get(path, params, **options)
+
+    def get_project_templates(self, params=None, **options):
+        """Get multiple project templates
+        :param Object params: Parameters for the request
+            - workspace {str}:  The workspace to filter results on.
+            - team {str}:  The team to filter projects on.
+        :param **options
+            - offset {str}:  Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. 'Note: You can only pass in an offset that was returned to you via a previously paginated request.'
+            - limit {int}:  Results per page. The number of objects to return per page. The value must be between 1 and 100.
+            - opt_fields {list[str]}:  Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options.
+            - opt_pretty {bool}:  Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging.
+        :return: Object
+        """
+        if params is None:
+            params = {}
+        path = "/project_templates"
+        return self.client.get_collection(path, params, **options)
+
+    def get_project_templates_for_team(self, team_gid, params=None, **options):
+        """Get a team's project templates
+        :param str team_gid: (required) Globally unique identifier for the team.
+        :param Object params: Parameters for the request
+        :param **options
+            - offset {str}:  Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. 'Note: You can only pass in an offset that was returned to you via a previously paginated request.'
+            - limit {int}:  Results per page. The number of objects to return per page. The value must be between 1 and 100.
+            - opt_fields {list[str]}:  Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options.
+            - opt_pretty {bool}:  Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging.
+        :return: Object
+        """
+        if params is None:
+            params = {}
+        path = "/teams/{team_gid}/project_templates".replace("{team_gid}", team_gid)
+        return self.client.get_collection(path, params, **options)
+
+    def instantiate_project(self, project_template_gid, params=None, **options):
+        """Instantiate a project from a project template
+        :param str project_template_gid: (required) Globally unique identifier for the project template.
+        :param Object params: Parameters for the request
+        :param **options
+            - opt_fields {list[str]}:  Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options.
+            - opt_pretty {bool}:  Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging.
+        :return: Object
+        """
+        if params is None:
+            params = {}
+        path = "/project_templates/{project_template_gid}/instantiateProject".replace("{project_template_gid}", project_template_gid)
+        return self.client.post(path, params, **options)
--- asana/resources/gen/projects.py
+++ asana/resources/gen/projects.py
@@ -212,6 +212,20 @@ class _Projects:
         path = "/projects/{project_gid}/task_counts".replace("{project_gid}", project_gid)
         return self.client.get(path, params, **options)
 
+    def project_save_as_template(self, project_gid, params=None, **options):
+        """Create a project template from a project
+        :param str project_gid: (required) Globally unique identifier for the project.
+        :param Object params: Parameters for the request
+        :param **options
+            - opt_fields {list[str]}:  Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options.
+            - opt_pretty {bool}:  Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging.
+        :return: Object
+        """
+        if params is None:
+            params = {}
+        path = "/projects/{project_gid}/saveAsTemplate".replace("{project_gid}", project_gid)
+        return self.client.post(path, params, **options)
+
     def remove_custom_field_setting_for_project(self, project_gid, params=None, **options):
         """Remove a custom field from a project
         :param str project_gid: (required) Globally unique identifier for the project.
